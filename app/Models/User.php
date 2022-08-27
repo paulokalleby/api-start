c67b73c0f6c9dc7d21dd4caa60982110f0d0a3d3
Paulo Kallleby <paulo.devweb@gmail.com>
2022-08-27 14:25:15 -0300

    Refactory HasUuidKey Trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Traits\UuidTrait;
 use App\Notifications\ResetPasswordNotification;
+use App\Traits\HasUuidKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,10 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, UuidTrait;
-
-    public $incrementing = false;
-    protected $keyType   = 'uuid';
+    use HasApiTokens, HasFactory, Notifiable, HasUuidKey;
 
     protected $fillable = [
         'name',
--- app/Traits/HasUuidKey.php
+++ app/Traits/HasUuidKey.php
@@ -4,8 +4,23 @@ namespace App\Traits;
 
 use Illuminate\Support\Str;
 
-trait UuidTrait 
+trait HasUuidKey 
 {
+    public function getKeyName()
+    {
+        return 'id';
+    }
+
+    public function getKeyType()
+    {
+        return 'uuid';
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+    
     public static function booted()
     {
         static::creating( function ($model) {
